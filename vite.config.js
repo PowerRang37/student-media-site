1c0e62f9331fa03dcd27ce55f538468ca2ad51bd
Aliaskar <aliaskar.tuzubekov@gmail.com>
2026-02-20 11:01:00 +0500

    deploy to a website

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/student-media-site/", // <-- название репо с слэшами
+  base: "/", // <-- название репо с слэшами
 });
